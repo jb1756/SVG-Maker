bbfb3c48f5735ea4a1269f6f4b6911efd8d98043
jb1756 <juliusbags04@yahoo.com>
2023-06-28 18:44:11 -0400

    worked out the errors on index.js and shapes.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const { Circle } = require("./lib/shapes");
-const { Triangle } = require("./lib/shapes");
-const { Square } = require("./lib/shapes");
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
 // Array of questions
 inquirer
   .prompt([
-    {/* Pass your questions in here */
-        name: 'text',
-        type: 'input',
-        message: "Enter 3 Characters:"
+    {
+      name: 'text',
+      type: 'input',
+      message: "Enter 3 Characters:",
+      validate: function (input) {
+        if (input.length > 3) {
+          return 'Only 3 Characters or less';
+        }
+        return true;
+      }
     },
 
     {
@@ -29,11 +33,13 @@ inquirer
         name: 'shapeColor',
         type:'input',
         message: 'What color of the shape? (Color or Hexadecimal)'
-
     }
 
-  ])
-  .then((answer) => {
+  ];
+
+
+  .then(answer => {
+    const {text, textColor, }
     // Use user feedback for... whatever!!
     if (answer.text.length > 3) {
         console.error('Only 3 Characters or less')
@@ -48,22 +54,48 @@ inquirer
     }
   });
 
-// ask questions, initialize to create logo using responses and catch errors.
-// 
 
+// Ask questions, initialize to create logo using responses, and catch errors.
 function init() {
-  inquirer.prompt(questions).then((response) => {
-    console.log(response);
+  inquirer.prompt(questions)
+    .then(response => {
+      const { text, textColor, shape, shapeColor } = response;
+      let logo;
 
-    const data = {
-      text: response.logotxt,
-      textColor: response.textcolor,
-      shape: response.shape,
-      shapeColor: response.shapeColor
-    };
-    fileWrite("logo-params.json", JSON.stringify(data, null, 1));
+      switch (shape) {
+        case 'Circle':
+          logo = new Circle(text, textColor, shapeColor);
+          break;
+        case 'Square':
+          logo = new Square(text, textColor, shapeColor);
+          break;
+        case 'Triangle':
+          logo = new Triangle(text, textColor, shapeColor);
+          break;
+      }
 
-  }
+      const data = {
+        text: text,
+        textColor: textColor,
+        shape: shape,
+        shapeColor: shapeColor
+      };
+
+      fs.writeFile("logo-params.json", JSON.stringify(data, null, 1), err => {
+        if (err) {
+          console.error('Error writing file:', err);
+        } else {
+          console.log('Logo parameters has been saved successfully!');
+        }
+      });
+    })
+    .catch(error => {
+      if (error.isTtyError) {
+        console.error('Prompt couldn\'t be rendered in the current environment');
+      } else {
+        console.error('Something else went wrong:', error);
+      }
+    });
 }
 
 init();
